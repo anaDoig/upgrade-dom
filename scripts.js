15187957fbb5422c0bf9bcb223fae690cc643eda
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-05-15 23:08:09 +0200

    Iteracion#1

---
+++ scripts.js
@@ -0,0 +1,14 @@
+/* Iteración 1 */
+
+window.onload = function() {
+    const testMe = document.querySelectorAll('[data-function="testMe"]');
+    const thirdChild = testMe[2];
+    console.log(document.querySelector('.showme'));
+    console.log(document.querySelector('#pillado'));
+    console.log(document.querySelectorAll('p'));
+    console.log(document.querySelectorAll('.pokemon'));
+    console.log(testMe);
+    console.log(thirdChild);
+};
+
+console.log('--------------');
